79d5515c62328ad2abb8d81afb492e4efacf29f8
SimonCastroV <142184921+SimonCastroV@users.noreply.github.com>
2025-08-06 15:03:17 -0500

    segundo tutorial terminado

--- pages/urls.py
+++ pages/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path 
-from .views import homePageView, AboutPageView, ProductIndexView, ProductShowView 
+from .views import homePageView, AboutPageView, ProductIndexView, ProductShowView,  ProductCreateView, TemplateView
   
 urlpatterns = [ 
      path("", homePageView.as_view(), name='home'), 
      path('about/', AboutPageView.as_view(), name='about'),
      path('products/', ProductIndexView.as_view(), name='index'),
      path('products/<str:id>', ProductShowView.as_view(), name='show'),
+     path('products/create/', ProductCreateView.as_view(), name='product-create'),
+     path('products/created/', TemplateView.as_view(template_name='products/created.html'), name='product-created'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,58 +1,97 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views import View
+from django.views.generic import TemplateView, ListView
+from django import forms
+from django.core.exceptions import ValidationError
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
-# Create your views here.
-from django.shortcuts import render # here by default 
-from django.views.generic import TemplateView
- # Create your views here. 
+from .models import Product
+
+
 class homePageView(TemplateView):
-   template_name = 'pages/home.html'  
+    template_name = 'pages/home.html'
 
 
-class AboutPageView(TemplateView): 
-    template_name = 'pages/about.html' 
- 
-     
-    def get_context_data(self, **kwargs): 
-        context = super().get_context_data(**kwargs) 
-        context.update({ 
-            "title": "About us - Online Store", 
-            "subtitle": "About us", 
-            "description": "This is an about page ...", 
-            "author": "Developed by: Your Name", 
-        }) 
- 
-        return context 
+class AboutPageView(TemplateView):
+    template_name = 'pages/about.html'
 
- 
-class Product: 
-    products = [ 
-        {"id":"1", "name":"TV", "description":"Best TV"}, 
-        {"id":"2", "name":"iPhone", "description":"Best iPhone"}, 
-        {"id":"3", "name":"Chromecast", "description":"Best Chromecast"}, 
-        {"id":"4", "name":"Glasses", "description":"Best Glasses"} 
-    ] 
- 
-class ProductIndexView(View): 
-    template_name = 'products/index.html' 
- 
-    def get(self, request): 
-        viewData = {} 
-        viewData["title"] = "Products - Online Store" 
-        viewData["subtitle"] =  "List of products" 
-        viewData["products"] = Product.products 
- 
-        return render(request, self.template_name, viewData) 
- 
-class ProductShowView(View): 
-    template_name = 'products/show.html' 
- 
- 
-    def get(self, request, id): 
-        viewData = {} 
-        product = Product.products[int(id)-1] 
-        viewData["title"] = product["name"] + " - Online Store" 
-        viewData["subtitle"] =  product["name"] + " - Product information" 
-        viewData["product"] = product 
- 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update({
+            "title": "About us - Online Store",
+            "subtitle": "About us",
+            "description": "This is an about page ...",
+            "author": "Developed by: Your Name",
+        })
+        return context
+
+
+class ProductIndexView(View):
+    template_name = 'products/index.html'
+
+    def get(self, request):
+        viewData = {}
+        viewData["title"] = "Products - Online Store"
+        viewData["subtitle"] = "List of products"
+        viewData["products"] = Product.objects.all()
         return render(request, self.template_name, viewData)
+
+
+class ProductShowView(View):
+    template_name = 'products/show.html'
+
+    def get(self, request, id):
+        # Check if product id is valid
+        try:
+            product_id = int(id)
+            if product_id < 1:
+                raise ValueError("Product id must be 1 or greater")
+            product = get_object_or_404(Product, pk=product_id)
+        except (ValueError, IndexError):
+            # If the product id is not valid, redirect to the home page
+            return HttpResponseRedirect(reverse('home'))
+
+        viewData = {}
+        viewData["title"] = product.name + " - Online Store"
+        viewData["subtitle"] = product.name + " - Product information"
+        viewData["product"] = product
+
+        return render(request, self.template_name, viewData)
+
+
+class ProductForm(forms.ModelForm):
+    name = forms.CharField(required=True)
+    price = forms.FloatField(required=True)
+
+    class Meta:
+        model = Product
+        fields = ['name', 'price']
+
+    def clean_price(self):
+        price = self.cleaned_data.get('price')
+        if price is not None and price <= 0:
+            raise ValidationError('Price must be greater than zero.')
+        return price
+
+
+class ProductCreateView(View):
+    template_name = 'products/create.html'
+
+    def get(self, request):
+        form = ProductForm()
+        viewData = {}
+        viewData["title"] = "Create product"
+        viewData["form"] = form
+        return render(request, self.template_name, viewData)
+
+    def post(self, request):
+        form = ProductForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('product-created')  # Asegúrate de tener esta ruta en urls.py
+        else:
+            viewData = {}
+            viewData["title"] = "Create product"
+            viewData["form"] = form
+            return render(request, self.template_name, viewData)
