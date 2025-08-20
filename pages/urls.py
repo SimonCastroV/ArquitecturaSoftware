9c382db197b1ef5b7cb64f0eee6f4e3189767388
SimonCastroV <142184921+SimonCastroV@users.noreply.github.com>
2025-08-20 09:03:18 -0500

    tutorial 5 con error al correr

--- pages/urls.py
+++ pages/urls.py
@@ -1,15 +1,25 @@
-from django.urls import path 
-from .views import homePageView, AboutPageView, ProductIndexView, ProductShowView,  ProductCreateView, TemplateView, CartView, CartRemoveAllView
-  
-urlpatterns = [ 
-     path("", homePageView.as_view(), name='home'), 
-     path('about/', AboutPageView.as_view(), name='about'),
-     path('products/', ProductIndexView.as_view(), name='index'),
-     path('products/<str:id>', ProductShowView.as_view(), name='show'),
-     path('products/create/', ProductCreateView.as_view(), name='product-create'),
-     path('products/created/', TemplateView.as_view(template_name='products/created.html'), name='product-created'),
-     path('cart/', CartView.as_view(), name='cart_index'),
-     path('cart/add/<str:product_id>', CartView.as_view(), name='cart_add'),
-     path('cart/removeAll', CartRemoveAllView.as_view(), name='cart_removeAll'),
+from django.urls import path
+from django.views.generic import TemplateView
+from .views import (
+    homePageView, 
+    AboutPageView, 
+    ProductIndexView, 
+    ProductShowView,  
+    ProductCreateView, 
+    CartView, 
+    CartRemoveAllView,
+)
 
+urlpatterns = [ 
+    path("", homePageView.as_view(), name='home'), 
+    path("about/", AboutPageView.as_view(), name='about'),
+    path("products/", ProductIndexView.as_view(), name='index'),
+    path("products/<str:id>", ProductShowView.as_view(), name='show'),
+    path("products/create/", ProductCreateView.as_view(), name='product-create'),
+    path("products/created/", TemplateView.as_view(template_name="products/created.html"), name="product-created"),
+    path("cart/", CartView.as_view(), name="cart_index"),
+    path("cart/add/<str:product_id>", CartView.as_view(), name="cart_add"),
+    path("cart/removeAll", CartRemoveAllView.as_view(), name="cart_removeAll"),
+    path("image/", ImageViewFactory(ImageLocalStorage()).as_view(), name="image_index"), 
+    path("image/save", ImageViewFactory(ImageLocalStorage()).as_view(), name="image_save"),
 ]
